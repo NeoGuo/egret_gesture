c7bdc67e2ae9d2e4e913a397a744e61fdc0a5d03
SHAORUI GUO <guoshaorui@gmail.com>
2016-03-30 08:02:50 +0800

    提交手势测试的Chrome插件

---
+++ ext/content/content_script.js
@@ -0,0 +1,18 @@
+﻿//MESSAGE
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        s = document.createElement("script");
+        if(request=="getMultiMode") {
+            s.innerHTML = "document.body.setAttribute('mm',window.getMultiMode())";
+            document.getElementsByTagName("head")[0].appendChild(s);
+            var mm = document.body.getAttribute("mm");
+            sendResponse(mm);
+        } else if(request=="removeMultiMode") {
+            s.innerHTML = "window.setMultiMode(window.currentThis,false)";
+            document.getElementsByTagName("head")[0].appendChild(s);
+        } else if(request=="setMultiMode") {
+            s.innerHTML = "window.setMultiMode(window.currentThis,true)";
+            document.getElementsByTagName("head")[0].appendChild(s);
+        }
+    }
+);
---
+++ ext/popup.js
@@ -0,0 +1,32 @@
+﻿/**获取当前页面*/
+function getCurrentTab(callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+    chrome.tabs.query(queryInfo, function (tabs) {
+        var tab = tabs[0];
+        callback(tab);
+    });
+}
+/**启动*/
+$(document).ready(function(){
+    //先获取到当前页面的Tab
+    getCurrentTab(function (tab) {
+        //然后给content_script发消息，获取游戏自身的设定
+        chrome.tabs.sendMessage(tab.id,"getMultiMode",function(response) {
+            if(response=="false") {
+                $("#se1").attr('checked','checked');
+            } else {
+                $("#se2").attr('checked','checked');
+            }
+        });
+        //点击单选按钮的时候，调用游戏自身的接口
+        $("#se1").click(function() {
+            chrome.tabs.sendMessage(tab.id,"removeMultiMode",function(response) {});
+        });
+        $("#se2").click(function() {
+            chrome.tabs.sendMessage(tab.id,"setMultiMode",function(response) {});
+        });
+    });
+});
